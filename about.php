324852c0ebd16093b836121f9900cb093a6b336c
Denis Mariga <42930751+DenisMariga@users.noreply.github.com>
2022-09-12 22:18:18 +0300

    Update about.php

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hostel website - About us</title>
+	 <title>meru slopes - About us</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta name="description" content="" />
 	<meta name="author" content="http://code4berry.com" />
